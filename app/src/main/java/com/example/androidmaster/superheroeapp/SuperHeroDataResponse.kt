100212bf87c018cbd251c2828bdc1b79a3695868
dam2 <franjimenezjimenez24@gmail.com>
2023-10-13 18:36:29 +0200

    Updated

--- app/src/main/java/com/example/androidmaster/superheroeapp/SuperHeroDataResponse.kt
+++ app/src/main/java/com/example/androidmaster/superheroeapp/SuperHeroDataResponse.kt
@@ -7,4 +7,9 @@ data class SuperHeroDataResponse(@SerializedName("response") val response: Strin
 }
 
 data class SuperHeroItemResponse(@SerializedName("id") val superHeroId: String,
-                                 @SerializedName("name") val name: String)
+                                 @SerializedName("name") val name: String,
+                                 @SerializedName("image") val superHeroImage: String)
+
+data class SuperHeroImageResponse(
+    @SerializedName("url") val url: String
+)
